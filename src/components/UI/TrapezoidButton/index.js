85a52ba9ca0b0c3e22e94f9b05c67e2fc5b960d3
David Arias <davidarias.devs@gmail.com>
2020-01-18 20:57:07 -0500

    Trapezoid button and page slider for home

---
+++ src/components/UI/TrapezoidButton/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from '@reach/router';
+
+import { navFont } from './../../../settings/mixins';
+
+const StyledTrapezoidButton = styled.svg`
+    width: 250px;
+    height: 100px;
+
+    ${ navFont }
+
+    font-size: 0.3em;
+
+    #Trapezoid-Front {
+        transform-origin: 50% 50%;
+        transform: translate(0,-5px) scaleX(1.3) rotateX(-10deg);
+        transition: transform 0.4s ease;
+    }
+
+    #Trapezoid-Back{
+        fill: transparent;
+        transition: fill 0.4s ease;
+    }
+
+    text {
+        fill: white;
+        transition: fill 0.4s ease;
+    }
+
+    &:hover {
+        #Trapezoid-Front{
+            transform: translate(0,0);
+        }
+
+        #Trapezoid-Back{
+            fill: white;
+        }
+        
+        text {
+            fill: black;
+        }
+    }
+`;
+
+const TrapezoidButton = ({ to, children }) => {
+    return (
+        <>
+            <StyledTrapezoidButton viewBox="0 0 100 40">
+                <a href={ to } style={{ color: 'white'}}>
+                    <path
+                        id="Trapezoid-Front"
+                        fill="transparent"
+                        stroke="white"
+                        strokeWidth="0.5" 
+                        d="M 15 10 L 85 10 L 75 30 L 25 30 z"
+                    />
+                    <path
+                        id="Trapezoid-Back"
+                        stroke="white"
+                        strokeWidth="0.5" 
+                        d="M 15 10 L 85 10 L 75 30 L 25 30 z"
+                    />
+                    <text x="50"
+                        y="20"
+                        textAnchor="middle"
+                        alignmentBaseline="middle">
+                    { children }
+                    </text>
+                </a>
+            </StyledTrapezoidButton>
+        </>
+    )
+}
+
+export default TrapezoidButton;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { FullPage, Slide } from 'react-full-page';
 import { connect } from 'react-redux';
 import { setCurrentSection } from '../../store/routing/actions.js';
 
 import './Home.scss'
 
 import Background from './../../components/Background';
+import TrapezoidButton from "../../components/UI/TrapezoidButton/index.js";
 
 class HomePage extends Component {
     componentDidMount() {
@@ -13,13 +15,42 @@ class HomePage extends Component {
 
     render(){
         return(
-            <div className="Container CenterContent">
-                <div className="PageContent">
-                    <h1 className="BigTitle">INFINITY WAR</h1>
-                    <h2>PREMIERE MUNDIAL <strong>23 DE ABRIL</strong> NO MÁS ESPERAS</h2>
-                    <a href="#watch_trailer">Ver Tráiler</a>
-                </div>
-                <Background src="Backgrounds/Wakanda.jpg"/>
+            <div className="Container">
+                <FullPage>
+                    <Slide>
+                        <div className="PageContent SlideContent">
+                            <h1 className="BigTitle">INFINITY WAR</h1>
+                            <h2>PREMIERE MUNDIAL <strong>23 DE ABRIL</strong> NO MÁS ESPERAS</h2>
+                            <TrapezoidButton to="trailer">VER TRAILER</TrapezoidButton>
+                            <Background src="Backgrounds/Wakanda.jpg"/>
+                        </div>
+                    </Slide>
+                    <Slide>
+                        <div className="PageContent SlideContent">
+                            <h1 className="BigTitle">THANOS</h1>
+                            <h2>El titán loco ha llegado a por las gemas</h2>
+                            <h2><strong>El conquistador de mundos</strong></h2>
+                            <h3>La película más esperada del año esta aquí</h3>
+                            <TrapezoidButton to="trailer">VER TRAILER</TrapezoidButton>
+                            <Background src="Backgrounds/Titan.jpg"/>
+                        </div>
+                    </Slide>
+                    <Slide>
+                        <div className="PageContent SlideContent">
+                            <h2>Nuevos equipos se unirán para luchar</h2>
+                            <h2>contra la <strong>inminente amenaza</strong></h2>
+                            <TrapezoidButton to="trailer">VER TRAILER</TrapezoidButton>
+                            <Background src="Backgrounds/GuardiansSpaceship.jpg"/>
+                        </div>
+                    </Slide>
+                    <Slide>
+                        <div className="PageContent SlideContent">
+                            <h2>Y harán todo lo posible para salvar el <strong>destino de la humanidad</strong></h2>
+                            <TrapezoidButton to="trailer">VER TRAILER</TrapezoidButton>
+                            <Background src="Backgrounds/NewYorkSanctum.jpg"/>
+                        </div>
+                    </Slide>
+                </FullPage>
             </div>
         );
     }
