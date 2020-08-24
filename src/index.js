53000849562c0cbfdf5bd67beff561aa6944489f
David Arias <davidarias.devs@gmail.com>
2020-08-23 21:19:10 -0500

    Packages update

--- src/index.js
+++ src/index.js
@@ -9,3 +9,13 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+        ReactDOM.render(
+            <NextApp />,
+            rootEl
+        )
+    })
+})
