00719d1476d2b14eb6673122ddcb0fca2a8d821a
David Arias <davidarias.devs@gmail.com>
2020-08-30 22:51:15 -0500

    Trailer VideoPlayer Expand

--- src/pages/Trailer/TrailerPage.js
+++ src/pages/Trailer/TrailerPage.js
@@ -1,37 +1,53 @@
-import React, { useEffect } from "react";
-import YouTube from 'react-youtube';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import YouTube from "react-youtube";
 
-// import styles from './Trailer.module.scss';
+import styles from "./Trailer.module.scss";
 
-import Background from './../../components/Background';
+import Background from "./../../components/Background";
 
 //TODO: El video debe ser responsive
 
 const TrailerPage = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
     const opts = {
-        height: '582',
-        width: '1400',
-        playerVars: { // https://developers.google.com/youtube/player_parameters
+        height: "582",
+        width: "1400",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
             enablejsapi: 1,
-            autoplay: 1
-        }
+            autoplay: 1,
+        },
     };
 
-    const _onReady = event => {
-        console.log('Ready');
+    const _onReady = (event) => {
+        console.log("Ready");
         event.target.pauseVideo();
-    }
+    };
 
-    return(
+    const _handlePlay = (event) => {
+        setIsExpanded(true);
+    };
+
+    const _handlePause = (event) => {
+        setIsExpanded(false);
+    };
+
+    return (
         <div className="Container Respect-TopBar CenterContent">
-            <YouTube
-                videoId="823iAZOEKt8"
-                opts={opts}
-                onReady={_onReady}
-            />
-            <Background src="Backgrounds/Poster.jpg"/>
+            <motion.div className={`${styles.VideoPlayer} ${!isExpanded ? styles.Closed : ""}`}>
+                <YouTube
+                    videoId="823iAZOEKt8"
+                    opts={opts}
+                    onPlay={_handlePlay}
+                    onReady={_onReady}
+                    onPause={_handlePause}
+                />
+            </motion.div>
+            <Background src="Backgrounds/Poster.jpg" />
         </div>
     );
-}
+};
 
 export default TrailerPage;
