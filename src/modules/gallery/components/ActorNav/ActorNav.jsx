08ac4f5723157e1997d8fc9469df33710837c24f
David Arias <davidarias.devs@gmail.com>
2022-11-07 19:20:52 -0500

    chore: framer motion, styled components and classnames dependencies bump

--- src/modules/gallery/components/ActorNav/ActorNav.jsx
+++ src/modules/gallery/components/ActorNav/ActorNav.jsx
@@ -6,66 +6,66 @@ import styles from "./ActorNav.module.scss";
 
 // Animation Variants
 const navVariants = {
-  visible: {
-    x: 0,
-    transition: {
-      staggerChildren: 0.3,
-      duration: 0.7,
-      type: "tween",
+    visible: {
+        x: 0,
+        transition: {
+            staggerChildren: 0.3,
+            duration: 0.7,
+            type: "tween",
+        },
     },
-  },
-  hidden: {
-    x: -800,
-    transition: {
-      staggerChildren: 0.3,
-      duration: 0.7,
-      type: "tween",
+    hidden: {
+        x: -800,
+        transition: {
+            staggerChildren: 0.3,
+            duration: 0.7,
+            type: "tween",
+        },
     },
-  },
 };
 
 const linkVariants = {
-  visible: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      delay: 0.5,
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            delay: 0.5,
+        },
     },
-  },
-  hidden: {
-    x: -10,
-    opacity: 0,
-    transition: {
-      type: "spring",
-      delay: 0.5,
+    hidden: {
+        x: -10,
+        opacity: 0,
+        transition: {
+            type: "spring",
+            delay: 0.5,
+        },
     },
-  },
 };
 
 const ActorNav = ({ id }) => {
-  return (
-    <motion.nav
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      variants={navVariants}
-      className={styles.Nav}
-      contentLabel="Galería de fotos"
-    >
-      <Link to={`/galeria/${id}/wallpapers`}>
-        <motion.div variants={linkVariants}>Wallpaper</motion.div>
-      </Link>
+    return (
+        <motion.nav
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={navVariants}
+            className={styles.Nav}
+            contentlabel="Galería de fotos"
+        >
+            <Link to={`/galeria/${id}/wallpapers`}>
+                <motion.div variants={linkVariants}>Wallpaper</motion.div>
+            </Link>
 
-      <Link to={`/galeria/${id}/social`}>
-        <motion.div variants={linkVariants}>Social</motion.div>
-      </Link>
+            <Link to={`/galeria/${id}/social`}>
+                <motion.div variants={linkVariants}>Social</motion.div>
+            </Link>
 
-      <Link to={`/galeria/${id}/fotos`}>
-        <motion.div variants={linkVariants}>Photos</motion.div>
-      </Link>
-    </motion.nav>
-  );
+            <Link to={`/galeria/${id}/fotos`}>
+                <motion.div variants={linkVariants}>Photos</motion.div>
+            </Link>
+        </motion.nav>
+    );
 };
 
 export default ActorNav;
